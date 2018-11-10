fb4e37f32bf7d2362499a3ba2f1a5b6fe2299b99
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-10 12:40:26 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame(`..Magic Server,`,'https://www.twitch.tv/MeeRcY')
+
+});
+client.login(process.env.BOT_TOKEN)
